6b07b0361da56ce9690693f09a0ad6799fe185f1
Darshan Hadiyal <146731392+imhadiyal@users.noreply.github.com>
2023-12-28 10:53:34 +0530

    28-12-2023

---
+++ pr-5/2.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+class A{
+	protected :
+		int age;
+
+		string error= "eligible for vote!!";
+		public :
+			A()
+			{	
+				cout << "enetr your age: ";
+				cin >> age;	
+				
+				try{
+					if(age>18){
+						throw error;
+					}
+					else{
+						throw true;
+					}			
+				}catch (bool e){
+					cout << endl << "you are not eligible for vote!!";
+				}catch(string error){
+					cout << error << endl;
+				}
+		}
+};
+int main(){
+	A a1;
+	
+}
